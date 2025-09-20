f5a594dceb7f5c21c4091a9682726cd915a8fe8a
rajendrazero <122064592+rajendrazero@users.noreply.github.com>
2025-09-20 23:03:12 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,17 +1,22 @@
-    <?php
+<?php
 class Database {
     public $conn;
     public function __construct() {
-        $host = "localhost";
+        $host = "maglev.proxy.rlwy.net";
+        $port = "35682"; // ganti sesuai port Railway DB-mu
         $db = "absensi_rohis";
         $user = "root";
-        $pass = "";
+        $pass = "RQDnCUbLBvpLHlbgfDDGuORKVjFOznhB";
+
         try {
-            $this->conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            $this->conn = new PDO(
+                "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+                $user,
+                $pass
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             die("Koneksi gagal: " . $e->getMessage());
         }
     }
 }
-
